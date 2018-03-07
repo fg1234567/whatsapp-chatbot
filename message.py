ec003cb6876f6ac8359d5cd0f01ff8575df392fb
furkaninalcik <m.furkaninalcik@gmail.com>
2018-03-07 13:15:48 +0300

    fixed bugs of the previous code

--- message.py
+++ message.py
@@ -7,51 +7,37 @@ import time
 driver = webdriver.Chrome()
 driver.get('http://web.whatsapp.com')
 
-
-#name = input('Enter the name of user or group : ')
-#msg = input('Enter the message : ')
-#count = int(input('Enter the count : '))
-
 #Scan the code before proceeding further
 input('Enter anything after scanning QR code')
-#user = driver.find_element_by_xpath('//div[@class = "_2wP_Y"]')
 
-search = driver.find_element_by_xpath('//*[@id="input-chatlist-search"]')
-search.send_keys("Halkman")
 
+#newConversation finds and clicks on the new conversation link on the main page
+newConversation = driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[2]/div')
+newConversation.click()
 time.sleep(5)
 
-user = driver.find_element_by_xpath('//*[@id="pane-side"]/div/div/div/div[2]')
-
-user.click()
-
+#searchPerson types the key into the search person space
+searchPerson = driver.find_element_by_xpath('//*[@id="input-chatlist-search"]')
+searchPerson.send_keys("Furkan")
 time.sleep(5)
 
+#selectPerson clicks on the first person on the list. Note: in this case there were only one person on the list.
+#It may cause problems if there were multiple
+#
+selectPerson = driver.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/div[1]/span/div/span/div/div[2]/div/div/div/div/div')
+selectPerson.click()
+time.sleep(5)
+
+#types the key into the message space inn the conversation page
 text_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
 
-msg_S = "This message has been sent by a simple bot! :)"
+msg_S = "."
 text_box.send_keys(msg_S)
-
-button = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/button')
-
 time.sleep(5)
 
-button.click()
+sendButton = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/button')
+sendButton.click()
+time.sleep(5)
+
+#button.click()
 print "Done!"
-
-'''
-msg2 = driver.find_element_by_xpath('//*[@id="main"]/div[2]/div/div/div[3]/div[16]/div/div/div/div[1]/div/span')
-
-text = msg2.text
-
-print "Last message you recieved is " + text
-'''
-
-
-'''
-msg_box = driver.find_element_by_class_name('input-container')
-
-for i in range(count):
-    msg_box.send_keys(msg)
-    #driver.find_element_by_class_name('compose-btn-send').click()
-'''
